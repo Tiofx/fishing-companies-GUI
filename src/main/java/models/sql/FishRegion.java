5e3f9bba6d98d8a184a17548d3d7fdd402b71db9
Tiofx <ioracle2718@gmail.com>
2016-11-29 19:00:23 +0300

    FishRegion sql model was created

---
+++ src/main/java/models/sql/FishRegion.java
@@ -0,0 +1,46 @@
+package models.sql;
+
+public class FishRegion implements ISqlModel {
+    public final static int PK_NUMBER = 1;
+    private int id;
+    private String placeName;
+    private String description;
+
+    public FishRegion() {
+        this(null, null);
+    }
+
+    public FishRegion(String placeName, String description) {
+        this(-1, placeName, description);
+    }
+
+    public FishRegion(int id, String placeName, String description) {
+        this.id = id;
+        this.placeName = placeName;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
