2a9807928e766739a13e76bd80043cbd983c3095
Tiofx <ioracle2718@gmail.com>
2016-11-29 11:46:14 +0300

    ShipController was created

--- src/main/java/controllers/ShipController.java
+++ src/main/java/controllers/ShipController.java
@@ -1,48 +1,43 @@
 package controllers;
 
-import models.sql.Fish;
+import models.sql.Ship;
 import models.table.BaseTableModel;
 
 import javax.sql.rowset.JdbcRowSet;
 import javax.swing.*;
 import java.sql.SQLException;
 
-public class FishController extends AbstractController<Fish> {
-
-    public FishController(JdbcRowSet jrs, BaseTableModel tableModel, JTable view) {
+public class ShipController extends AbstractController<Ship> {
+    public ShipController(JdbcRowSet jrs, BaseTableModel tableModel, JTable view) {
         super(jrs, tableModel, view);
     }
 
     @Override
-    protected void updateRecord(Fish newRecord) throws SQLException {
+    protected void updateRecord(Ship newRecord) throws SQLException {
         if (newRecord.getId() != -1) {
             jrs.updateInt(1, newRecord.getId());
         }
         jrs.updateString(2, newRecord.getName());
-        jrs.updateInt(3, newRecord.getPrice());
     }
 
     @Override
-    protected Fish getRecord() throws SQLException {
-        return new Fish(jrs.getInt(1), jrs.getString(2), jrs.getInt(3));
+    protected Ship getRecord() throws SQLException {
+        return new Ship(jrs.getInt(1), jrs.getString(2));
     }
 
     @Override
     protected int getSkipNumber() {
-        return Fish.PK_NUMBER;
+        return Ship.PK_NUMBER;
     }
 
     @Override
-    protected void setCommandParameter(int numParameter, int numModelField, Fish searchFields) throws SQLException {
+    protected void setCommandParameter(int numParameter, int numModelField, Ship searchFields) throws SQLException {
         switch (numModelField) {
             case 1:
                 jrs.setInt(numParameter, searchFields.getId());
                 break;
             case 2:
                 jrs.setString(numParameter, searchFields.getName());
-                break;
-            case 3:
-                jrs.setInt(numParameter, searchFields.getPrice());
                 break;
             default:
                 throw new SQLException();
