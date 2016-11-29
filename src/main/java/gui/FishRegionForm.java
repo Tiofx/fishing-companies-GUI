1afdef49fc9f539f6b68c11474b9646e10783376
Tiofx <ioracle2718@gmail.com>
2016-11-29 19:46:19 +0300

    FishRegionForm was created

---
+++ src/main/java/gui/FishRegionForm.java
@@ -0,0 +1,60 @@
+package gui;
+
+import models.sql.FishRegion;
+
+import javax.swing.*;
+
+public class FishRegionForm extends JPanel implements IUniversalForm<FishRegion> {
+    private JPanel rootPanel;
+    private JTextField placeNameTxt;
+    private JTextField descriptionTxt;
+
+    public FishRegionForm() {
+        super();
+        add(rootPanel);
+    }
+
+    @Override
+    public FishRegion getRecord() {
+        return new FishRegion(getPlaceName(), getDescription());
+    }
+
+    @Override
+    public FishRegion getRawRecord() {
+        FishRegion fishRegion = new FishRegion();
+
+        if (canGetPlaceName())
+            fishRegion.setPlaceName(getPlaceName());
+        if (canGetDescription())
+            fishRegion.setDescription(getDescription());
+
+        return fishRegion;
+    }
+
+    @Override
+    public void setRecord(FishRegion record) {
+        placeNameTxt.setText(record.getPlaceName());
+        descriptionTxt.setText(record.getDescription());
+    }
+
+    @Override
+    public Boolean[] canGets() {
+        return new Boolean[]{canGetPlaceName(), canGetDescription()};
+    }
+
+    public boolean canGetPlaceName() {
+        return getPlaceName().length() > 0;
+    }
+
+    public boolean canGetDescription() {
+        return getDescription().length() > 0;
+    }
+
+    public String getPlaceName() {
+        return placeNameTxt.getText();
+    }
+
+    public String getDescription() {
+        return descriptionTxt.getText();
+    }
+}
