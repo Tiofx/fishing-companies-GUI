163baa00da65ae6c50d2331c9e6a9b74e72aa000
Tiofx <ioracle2718@gmail.com>
2016-11-29 20:26:44 +0300

    FishSeason sql model was created

---
+++ src/main/java/models/sql/FishSeason.java
@@ -0,0 +1,58 @@
+package models.sql;
+
+import java.sql.Date;
+
+public class FishSeason implements ISqlModel {
+    public final static int PK_NUMBER = 1;
+    private int id;
+    private int fishRegionId;
+    private java.sql.Date begin;
+    private java.sql.Date end;
+
+    public FishSeason() {
+        this(0, null, null);
+    }
+
+    public FishSeason(int fishRegionId, Date begin, Date end) {
+        this(-1, fishRegionId, begin, end);
+    }
+
+    public FishSeason(int id, int fishRegionId, Date begin, Date end) {
+        this.id = id;
+        this.fishRegionId = fishRegionId;
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFishRegionId() {
+        return fishRegionId;
+    }
+
+    public void setFishRegionId(int fishRegionId) {
+        this.fishRegionId = fishRegionId;
+    }
+
+    public Date getBegin() {
+        return begin;
+    }
+
+    public void setBegin(Date begin) {
+        this.begin = begin;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+}
