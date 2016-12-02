9a9ae63accaac70a760192a05ab0699c886e33d3
Tiofx <ioracle2718@gmail.com>
2016-12-02 13:10:17 +0300

    add & edit option in QuotaForm (without confirmation)

--- src/main/java/gui/QuotaForm.java
+++ src/main/java/gui/QuotaForm.java
@@ -2,23 +2,80 @@ package gui;
 
 import controllers.FishRegionController;
 import models.gui.FishRegionComboBoxModel;
+import models.sql.FishRegion;
 import models.sql.Quota;
 
 import javax.sql.rowset.JdbcRowSet;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static javax.swing.JOptionPane.showConfirmDialog;
 
 public class QuotaForm extends JPanel implements IUniversalForm<Quota> {
     private JPanel rootPanel;
     private JTextField yearTxt;
     private JComboBox placeNameCB;
+    private JButton editButton;
 
     private FishRegionController fishRegion;
+
+    private class ComboRenderer extends DefaultListCellRenderer {
+
+        @Override
+        public Component getListCellRendererComponent(JList list,
+                                                      Object value, int index, boolean isSelected, boolean cellHasFocus) {
+            JComponent c = (JComponent) super.getListCellRendererComponent(list,
+                    value, index, isSelected, cellHasFocus);
+
+            if (value instanceof FishRegion && !isSelected) {
+                setText(((FishRegion) value).getPlaceName());
+            }
+
+            if (value instanceof FishRegion && isSelected) {
+//                list.setToolTipText(value.toString());
+//                setText(((FishRegion) value).getPlaceName());
+
+                setText(value.toString());
+            } else if (!(value instanceof FishRegion) && isSelected) {
+                list.setToolTipText("none :(");
+            }
+            return c;
+        }
+    }
 
 
     public QuotaForm(FishRegionController fishRegion) {
         super();
         add(rootPanel);
         this.fishRegion = fishRegion;
+
+        placeNameCB.setRenderer(new ComboRenderer());
+        editButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                FishRegion fr = (FishRegion) placeNameCB.getSelectedItem();
+
+                FishRegionForm inputPanel = new FishRegionForm();
+                inputPanel.setRecord(fr);
+                String nameOperation = "add";
+                int result = showConfirmDialog(null,
+                        inputPanel,
+                        nameOperation + " form",
+                        JOptionPane.OK_CANCEL_OPTION,
+                        JOptionPane.PLAIN_MESSAGE);
+
+                if (result == JOptionPane.OK_OPTION) {
+                    if (inputPanel.canGetRecord()) {
+                        FishRegion record = inputPanel.getRecord();
+
+                        placeNameCB.setSelectedItem(record);
+                    }
+                }
+
+            }
+        });
     }
 
     public QuotaForm(JdbcRowSet jrs) {
--- src/main/java/models/gui/FishRegionComboBoxModel.java
+++ src/main/java/models/gui/FishRegionComboBoxModel.java
@@ -1,13 +1,15 @@
 package models.gui;
 
+import gui.FishRegionForm;
 import models.sql.FishRegion;
 
 import javax.sql.rowset.JdbcRowSet;
 import javax.swing.*;
-import javax.swing.event.ListDataListener;
 import java.sql.SQLException;
 
-public class FishRegionComboBoxModel implements ComboBoxModel<FishRegion> {
+import static javax.swing.JOptionPane.showConfirmDialog;
+
+public class FishRegionComboBoxModel extends AbstractListModel<FishRegion> implements ComboBoxModel<FishRegion> {
     protected JdbcRowSet objects;
     protected FishRegion selectedObject;
 
@@ -15,6 +17,7 @@ public class FishRegionComboBoxModel implements ComboBoxModel<FishRegion> {
         this.objects = objects;
     }
 
+    // TODO: 02/12/2016 refactoring!!!!!
     @Override
     public void setSelectedItem(Object anItem) {
         if (anItem != null && anItem instanceof Integer) {
@@ -27,20 +30,72 @@ public class FishRegionComboBoxModel implements ComboBoxModel<FishRegion> {
                         break;
                     }
                 }
+                fireContentsChanged(this, -1, -1);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
             return;
         }
+        if (anItem != null && !(anItem instanceof FishRegion)) {
+            // TODO: 02/12/2016 delete (this from MainForm add option)
+            int i = JOptionPane.showConfirmDialog(null,
+                    "This record doesn't exist yet.\n Would you like to add this record?",
+                    "add?",
+                    JOptionPane.OK_OPTION,
+                    JOptionPane.PLAIN_MESSAGE);
+
+            if (i != JOptionPane.OK_OPTION) {
+                return;
+            }
+            FishRegionForm inputPanel = new FishRegionForm();
+            inputPanel.setRecord(new FishRegion((String) anItem, ""));
+            String nameOperation = "add";
+            int result = showConfirmDialog(null,
+                    inputPanel,
+                    nameOperation + " form",
+                    JOptionPane.OK_CANCEL_OPTION,
+                    JOptionPane.PLAIN_MESSAGE);
+
+            if (result == JOptionPane.OK_OPTION) {
+                if (inputPanel.canGetRecord()) {
+                    FishRegion record = inputPanel.getRecord();
+
+                    try {
+                        objects.moveToInsertRow();
+
+                        objects.updateString(2, record.getPlaceName());
+                        objects.updateString(3, record.getDescription());
+
+                        objects.insertRow();
+                        objects.beforeFirst();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            fireContentsChanged(this, -1, -1);
+            return;
+        }
+
         if ((selectedObject != null && !selectedObject.equals(anItem)) ||
                 selectedObject == null && anItem != null) {
             selectedObject = (FishRegion) anItem;
+            try {
+                objects.updateString(2, selectedObject.getPlaceName());
+                objects.updateString(3, selectedObject.getDescription());
+                objects.updateRow();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+//            JOptionPane.showConfirmDialog(null, "edit this?");
+            fireContentsChanged(this, -1, -1);
         }
     }
 
     @Override
     public Object getSelectedItem() {
-        return selectedObject.getId();
+        return selectedObject;
     }
 
     @Override
@@ -56,6 +111,8 @@ public class FishRegionComboBoxModel implements ComboBoxModel<FishRegion> {
 
     @Override
     public FishRegion getElementAt(int index) {
+        if (index == -1)
+            return null;
         try {
             objects.absolute(index + 1);
             FishRegion fishRegion = new FishRegion(objects.getInt(1), objects.getString(2), objects.getString(3));
@@ -66,13 +123,4 @@ public class FishRegionComboBoxModel implements ComboBoxModel<FishRegion> {
         }
     }
 
-    @Override
-    public void addListDataListener(ListDataListener l) {
-
-    }
-
-    @Override
-    public void removeListDataListener(ListDataListener l) {
-
-    }
 }
