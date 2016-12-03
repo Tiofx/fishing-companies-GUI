9949746696d7979fc93248f07e12b518707a6463
Tiofx <ioracle2718@gmail.com>
2016-12-03 17:22:21 +0300

    Factory class was created

---
+++ src/main/java/unit/Factory.java
@@ -0,0 +1,35 @@
+package unit;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class Factory<T, R> {
+    protected final Map<T, Supplier<? extends R>> creators = new HashMap<>();
+
+    public R getForm(T id) {
+        if (creators.containsKey(id)) {
+            return creators.get(id).get();
+        } else throw new NotImplementedException();
+    }
+
+    public boolean add(T id, Supplier<? extends R> creator) {
+        if (creators.containsKey(id)) {
+            return false;
+        }
+        creators.put(id, creator);
+
+        return true;
+    }
+
+    public boolean remove(T id) {
+        if (creators.containsKey(id)) {
+            creators.remove(id);
+            return true;
+        }
+
+        return false;
+    }
+}
