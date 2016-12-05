d93f3199dc690c5bba2c689007d0527916a680a8
Tiofx <ioracle2718@gmail.com>
2016-12-05 17:18:48 +0300

    refactoring FishRegionComboBoxModel

--- src/main/java/models/gui/FishRegionComboBoxModel.java
+++ src/main/java/models/gui/FishRegionComboBoxModel.java
@@ -1,43 +1,39 @@
 package models.gui;
 
+import controllers.FishRegionController;
 import gui.FishRegionForm;
 import models.sql.FishRegion;
+import unit.Dialog;
 
 import javax.sql.rowset.JdbcRowSet;
 import javax.swing.*;
 import java.sql.SQLException;
 
-import static javax.swing.JOptionPane.showConfirmDialog;
-
 public class FishRegionComboBoxModel extends AbstractListModel<FishRegion> implements ComboBoxModel<FishRegion> {
-    protected JdbcRowSet objects;
+    protected FishRegionController objects;
     protected FishRegion selectedObject;
 
     public FishRegionComboBoxModel(JdbcRowSet objects) {
-        this.objects = objects;
+        this.objects = new FishRegionController(objects);
     }
 
-    // TODO: 02/12/2016 refactoring!!!!!
     @Override
     public void setSelectedItem(Object anItem) {
         if (anItem != null && anItem instanceof Integer) {
-            try {
-                int pk = (int) anItem;
-                objects.beforeFirst();
-                while (objects.next()) {
-                    if (objects.getInt(1) == pk) {
-                        selectedObject = new FishRegion(objects.getInt(1), objects.getString(2), objects.getString(3));
-                        break;
-                    }
-                }
-                fireContentsChanged(this, -1, -1);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            Boolean[] mask = {true, false, false};
+            FishRegion find = new FishRegion();
+
+            find.setId((int) anItem);
+            objects.fullFind(mask, find);
+
+            selectedObject = objects.getRecord(1);
+
+            objects.reset();
+            fireContentsChanged(this, -1, -1);
             return;
         }
         if (anItem != null && !(anItem instanceof FishRegion)) {
-            // TODO: 02/12/2016 delete (this from MainForm add option)
+
             int i = JOptionPane.showConfirmDialog(null,
                     "This record doesn't exist yet.\n Would you like to add this record?",
                     "add?",
@@ -49,30 +45,9 @@ public class FishRegionComboBoxModel extends AbstractListModel<FishRegion> imple
             }
             FishRegionForm inputPanel = new FishRegionForm();
             inputPanel.setRecord(new FishRegion((String) anItem, ""));
-            String nameOperation = "add";
-            int result = showConfirmDialog(null,
-                    inputPanel,
-                    nameOperation + " form",
-                    JOptionPane.OK_CANCEL_OPTION,
-                    JOptionPane.PLAIN_MESSAGE);
 
-            if (result == JOptionPane.OK_OPTION) {
-                if (inputPanel.canGetRecord()) {
-                    FishRegion record = inputPanel.getRecord();
-
-                    try {
-                        objects.moveToInsertRow();
-
-                        objects.updateString(2, record.getPlaceName());
-                        objects.updateString(3, record.getDescription());
-
-                        objects.insertRow();
-                        objects.beforeFirst();
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
+            Dialog.makeOperation("add", inputPanel, (fr -> objects.insert(fr)));
+//            selectedObject =
 
             fireContentsChanged(this, -1, -1);
             return;
@@ -81,14 +56,7 @@ public class FishRegionComboBoxModel extends AbstractListModel<FishRegion> imple
         if ((selectedObject != null && !selectedObject.equals(anItem)) ||
                 selectedObject == null && anItem != null) {
             selectedObject = (FishRegion) anItem;
-            try {
-                objects.updateString(2, selectedObject.getPlaceName());
-                objects.updateString(3, selectedObject.getDescription());
-                objects.updateRow();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-//            JOptionPane.showConfirmDialog(null, "edit this?");
+            objects.edit(selectedObject);
             fireContentsChanged(this, -1, -1);
         }
     }
@@ -101,10 +69,9 @@ public class FishRegionComboBoxModel extends AbstractListModel<FishRegion> imple
     @Override
     public int getSize() {
         try {
-            objects.last();
-            return objects.getRow();
+            objects.getJrs().last();
+            return objects.getJrs().getRow();
         } catch (SQLException e) {
-            e.printStackTrace();
             return 0;
         }
     }
@@ -114,13 +81,10 @@ public class FishRegionComboBoxModel extends AbstractListModel<FishRegion> imple
         if (index == -1)
             return null;
         try {
-            objects.absolute(index + 1);
-            FishRegion fishRegion = new FishRegion(objects.getInt(1), objects.getString(2), objects.getString(3));
-            return fishRegion;
-        } catch (SQLException e) {
+            return objects.getRecord(index + 1);
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
-
 }
