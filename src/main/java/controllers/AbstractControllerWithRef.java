31824062e853f409776f098f67a37a2c8075520e
Tiofx <ioracle2718@gmail.com>
2016-12-07 11:47:25 +0300

    AbstractControllerWithRef was created
    controller for table that have one FK

---
+++ src/main/java/controllers/AbstractControllerWithRef.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import models.gui.BaseTableModel;
+import models.gui.QuotaTableModel;
+import unit.Connection;
+
+import javax.sql.rowset.JdbcRowSet;
+
+public abstract class AbstractControllerWithRef<T> extends AbstractController<T> {
+    protected JdbcRowSet reference;
+
+    public AbstractControllerWithRef() {
+    }
+
+    public AbstractControllerWithRef(Connection connection, String tableName) {
+        super(connection, tableName);
+    }
+
+    public AbstractControllerWithRef(Connection connection, String tableName, String refTableName) {
+        this.reference = connection.getJRS(refTableName);
+        setJrs(connection.getJRS(tableName));
+    }
+
+    public AbstractControllerWithRef(JdbcRowSet jrs) {
+        super(jrs);
+    }
+
+    @Override
+    protected BaseTableModel createTableModel(JdbcRowSet jrs) {
+        return new QuotaTableModel(jrs, reference);
+    }
+}
