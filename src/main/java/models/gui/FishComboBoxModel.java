8ed49e76cc1012e94f65f5cd3103d314dd8f27d7
Tiofx <ioracle2718@gmail.com>
2016-12-08 15:26:07 +0300

    FishCombBoxModel was created

---
+++ src/main/java/models/gui/FishComboBoxModel.java
@@ -0,0 +1,50 @@
+package models.gui;
+
+import controllers.AbstractController;
+import models.sql.Fish;
+import models.sql.FishRegion;
+
+public class FishComboBoxModel extends AbstractSqlComboBoxModel<Fish> {
+    public FishComboBoxModel(AbstractController<Fish> objects) {
+        super(objects);
+    }
+
+    @Override
+    public void findBy(String field) {
+        objects.fullFind(new Boolean[]{false, true, false},
+                new Fish(field, 0));
+    }
+
+    @Override
+    public void setSelectedItem(Object anItem) {
+        if (anItem != null) {
+            if (anItem instanceof FishRegion) {
+                if ((selectedObject != null
+                        && !selectedObject.equals(anItem)) ||
+                        selectedObject == null) {
+
+                    selectedObject = (Fish) anItem;
+                    fireContentsChanged(this, -1, -1);
+                }
+            } else if (anItem instanceof Integer) {
+                objects.fullFind(new Boolean[]{true, false, false},
+                        new Fish((int) anItem, null, 0));
+
+                selectedObject = objects.getRecord(1);
+
+                objects.reset();
+                fireContentsChanged(this, -1, -1);
+            } else {
+                objects.fullFind(new Boolean[]{false, true, false},
+                        new Fish((String) anItem, 0));
+
+                if (objects.getRecord(1) != null) {
+                    selectedObject = objects.getRecord(1);
+
+                    objects.reset();
+                    fireContentsChanged(this, -1, -1);
+                }
+            }
+        }
+    }
+}
